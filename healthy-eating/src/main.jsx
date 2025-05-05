b6961e23dc00e9ba13c9beed713b3710495f0e29
KalkabaevaInkara <inkara18082008@gmail.com>
2025-05-06 02:53:34 +0500

    Change router to HashRouter for better URL handling

--- healthy-eating/src/main.jsx
+++ healthy-eating/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/global.css";
 import "./styles/typography.css"
@@ -10,8 +10,8 @@ import "./styles/adaptation.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
